5cdd204b9d722ab72778a7a437a6d5adfb831994
Baha Brahim <bahbouhabrahim2@gmail.com>
2024-08-28 07:08:03 -0700

    design is done , started working on js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+    // Animation for the player1, vs and player2
     const player1 = document.querySelector('.player1');
     const vs = document.querySelector('.vs-container');
     const player2 = document.querySelector('.player2');
@@ -17,4 +18,14 @@ window.addEventListener('load', () => {
         player2.classList.remove('hidden');
         player2.classList.add('player2-animation');
     }, 2000);
+
+    // Changing the diffiulty level on click
+    const level = ['Low Elo', 'Mid Elo', 'High Elo'];
+    const levelBtn = document.querySelectorAll('.drop-down-diff');
+    levelBtn.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            document.querySelector('p').textContent = level[index];
+        });
+        index++;
+    });
 });
